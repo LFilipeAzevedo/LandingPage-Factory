6773d3295c708b542997fb8995337258c43512a1
Azevedo <luiz.azevedo@portaldecompraspublicas.com.br>
2026-02-07 19:30:37 -0300

    chore: atualiza script de debug de e-mail para Gmail corporativo

--- server/debug_email.js
+++ server/debug_email.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 async function test() {
     console.log('🚀 Iniciando teste de e-mail...');
-    const result = await sendVerificationEmail('filipedonofre@gmail.com', 'Teste Debug', 'token-debug-123');
+    const result = await sendVerificationEmail('LPFactory.Dev@gmail.com', 'Teste Debug Gmail', 'token-debug-123');
     console.log('Result:', JSON.stringify(result, null, 2));
 }
 
